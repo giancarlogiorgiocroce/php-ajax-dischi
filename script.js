bd40ffe37883092b2aabe9044e59ef836efa64dd
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-06-09 17:37:31 +0200

    Aggiunto Header e prime prove con la creazione dinamica delle option per il genere

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const app = new Vue ({
         apiUrl: 'http://localhost/boolean-ex/php-ajax-dischi/api.php',
         apiArray: [],
 
+        genreValue: '',
+        genreArray: [],
+
     },
     mounted(){
         this.getApi();
@@ -13,7 +16,20 @@ const app = new Vue ({
             axios.get(this.apiUrl)
             .then(res => {
                 this.apiArray = res.data;
-            })
-        }
+            });
+            // this.genreArrayGenerator();
+        },
+        // genreArrayGenerator(){
+        //     if(this.apiArray.length > 0){
+        //         this.apiArray.forEach(obj => {
+        //             console.log(obj);
+        //             if(!this.genreArray.includes(obj.genre)){
+        //                 this.genreArray.push(obj.genre);
+        //             }
+        //         });
+        //     }else if(this.apiArray.length == 0){
+        //         console.log('oh no');
+        //     }
+        // },
     },
 })
