9f23bf6bfab2a9d55dc2a8a829d19610fdc59be0
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-06-09 16:57:39 +0200

    Milestone 2 completato

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const app = new Vue ({
     el: '#app',
     data:{
+        apiUrl: 'http://localhost/boolean-ex/php-ajax-dischi/api.php',
+        apiArray: [],
 
     },
-    mounted:{
-
+    mounted(){
+        this.getApi();
     },
     methods:{
-        
-    }
+        getApi(){
+            axios.get(this.apiUrl)
+            .then(res => {
+                this.apiArray = res.data;
+            })
+        }
+    },
 })
